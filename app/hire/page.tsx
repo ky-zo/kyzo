64e5260eac9e14d91b83de631345157ea738660a
Kamil <zowczak.kamil@gmail.com>
2023-08-12 18:12:43 +0200

    fixes

--- app/hire/page.tsx
+++ app/hire/page.tsx
@@ -7,6 +7,9 @@ import { v4 as uuidv4 } from 'uuid'
 
 import { PiArrowLeft } from 'react-icons/pi'
 import { CgSpinnerAlt } from 'react-icons/cg'
+import { AiOutlineCheck } from 'react-icons/ai'
+
+import { useRouter } from 'next/navigation'
 
 const contactOptions = [
   {
@@ -20,27 +23,42 @@ const contactOptions = [
     id: uuidv4(),
     name: 'email',
     link: 'mailto:kamil@kyzo.io',
-    description: 'opens email client',
+    description: 'copy email or double-click to open email client',
     loading: false,
   },
 ]
 
 const Page = () => {
   const [options, setOptions] = useState(contactOptions)
+  const [emailCopied, setEmailCopied] = useState(false)
+  const router = useRouter()
 
-  const handleClick = (id: string) => {
-    setOptions((prev) => {
-      return prev.map((option) => {
-        if (option.id === id) {
-          return {
-            ...option,
-            loading: true,
+  const handleClick = (picked: any) => {
+    if (picked.name === 'async call') {
+      setOptions((prev) => {
+        return prev.map((option) => {
+          if (option.id === picked.id) {
+            return {
+              ...option,
+              loading: true,
+            }
+          } else {
+            return option
           }
-        } else {
-          return option
-        }
+        })
       })
-    })
+      router.push(picked.link)
+      return
+    }
+
+    if (emailCopied) {
+      router.push(picked.link)
+      return
+    }
+
+    navigator.clipboard.writeText('kamil@kyzo.io')
+    setEmailCopied(true)
+    console.log('email copied')
   }
 
   return (
@@ -59,9 +77,8 @@ const Page = () => {
                 <div
                   key={option.id}
                   className="w-full">
-                  <a
-                    onClick={() => handleClick(option.id)}
-                    href={option.link}
+                  <button
+                    onClick={() => handleClick(option)}
                     className={`group peer relative flex h-8 w-full items-center justify-center overflow-hidden rounded-xl border-[1px] border-gray-500 bg-black font-medium text-white transition-all duration-300 ${
                       !option.loading && `hover:bg-white`
                     } sm:min-w-[120px]`}>
@@ -72,8 +89,12 @@ const Page = () => {
                         {option.name}
                       </span>
                     )}
-                  </a>
-                  <p className="invisible absolute -bottom-6 translate-x-full text-xs text-gray-500 opacity-0 transition-all duration-300 peer-hover:visible peer-hover:translate-x-0 peer-hover:opacity-100">
+                    {option.name === 'email' && emailCopied && (
+                      <AiOutlineCheck className="absolute right-4 pl-1 transition-all duration-300 group-hover:invert" />
+                    )}
+                  </button>
+
+                  <p className="invisible absolute -bottom-8  w-full max-w-[200px] translate-x-full text-xs text-gray-500 opacity-0 transition-all duration-300 peer-hover:visible peer-hover:translate-x-0 peer-hover:opacity-100 sm:-bottom-6 sm:min-w-[400px]">
                     {option.description}
                   </p>
                 </div>
