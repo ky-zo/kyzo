e446c38f6cb0e0760046c1558a648c975204ccae
Kamil <zowczak.kamil@gmail.com>
2025-09-01 19:38:48 -0700

    change failed to closed

--- app/page.tsx
+++ app/page.tsx
@@ -1,46 +1,66 @@
-import { Badge } from '@/components/ui/badge'
-import { exp } from '@/content/exp'
-import { cn } from '@/lib/utils'
+import { Metadata } from "next";
+
+import { Badge } from "@/components/ui/badge";
+import { exp } from "@/content/exp";
+import { cn } from "@/lib/utils";
+
+export const metadata: Metadata = {
+	title: "kyzo",
+	description: "kyzo.io",
+	alternates: {
+		canonical: "https://kyzo.io",
+	},
+};
 
 export default function Home() {
-  return (
-    <ul className="w-full max-w-md space-y-5">
-      {exp.map((e) => {
-        return (
-          <li
-            key={e.id}
-            className={cn('shadow-neumorphic flex min-h-[50px] w-full flex-col gap-2 rounded-2xl border-[0.5px] p-4', {
-              'bg-gray-50': e.status === 'quit' || e.status === 'failed',
-            })}>
-            <div className="flex flex-col items-start">
-              <div className="flex w-full items-center justify-between">
-                <div>
-                  {e.title && e.title + '@'}
-                  <a
-                    href={e.company.href}
-                    className="font-bold hover:underline"
-                    target="_blank">
-                    {e.company.name}
-                  </a>
-                </div>
-                <Badge
-                  variant={'outline'}
-                  className={cn('font-normal', {
-                    'border-red-300 bg-red-50 text-red-500': e.status === 'failed',
-                    'border-green-300 bg-green-50 text-green-500': e.status === 'acquired',
-                    'border-blue-300 bg-blue-50 text-blue-500': e.status === 'active',
-                    'border-gray-300 bg-gray-50 text-gray-500': e.status === 'quit',
-                  })}>
-                  {e.status}
-                </Badge>
-              </div>
-            </div>
-            <div className="flex flex-col-reverse gap-2 sm:flex-row">
-              <span className="text-xs text-black/50">{e.oneLiner}</span>
-            </div>
-          </li>
-        )
-      })}
-    </ul>
-  )
+	return (
+		<ul className="w-full max-w-md space-y-5">
+			{exp.map((e) => {
+				return (
+					<li
+						key={e.id}
+						className={cn(
+							"shadow-neumorphic flex min-h-[50px] w-full flex-col gap-2 rounded-2xl border-[0.5px] p-4",
+							{
+								"bg-gray-50": e.status === "quit" || e.status === "closed",
+							},
+						)}
+					>
+						<div className="flex flex-col items-start">
+							<div className="flex w-full items-center justify-between">
+								<div>
+									{e.title && e.title + "@"}
+									<a
+										href={e.company.href}
+										className="font-bold hover:underline"
+										target="_blank"
+									>
+										{e.company.name}
+									</a>
+								</div>
+								<Badge
+									variant={"outline"}
+									className={cn("font-normal", {
+										"border-red-300 bg-red-50 text-red-500":
+											e.status === "closed",
+										"border-green-300 bg-green-50 text-green-500":
+											e.status === "acquired",
+										"border-blue-300 bg-blue-50 text-blue-500":
+											e.status === "active",
+										"border-gray-300 bg-gray-50 text-gray-500":
+											e.status === "quit",
+									})}
+								>
+									{e.status}
+								</Badge>
+							</div>
+						</div>
+						<div className="flex flex-col-reverse gap-2 sm:flex-row">
+							<span className="text-xs text-black/50">{e.oneLiner}</span>
+						</div>
+					</li>
+				);
+			})}
+		</ul>
+	);
 }
--- content/exp.ts
+++ content/exp.ts
@@ -1,101 +1,101 @@
-import { v4 as uuidv4 } from 'uuid'
+import { v4 as uuidv4 } from "uuid";
 
 type expProps = {
-  id: string
-  dates: string
-  title?: string
-  company: {
-    name: string
-    href: string
-  }
+	id: string;
+	dates: string;
+	title?: string;
+	company: {
+		name: string;
+		href: string;
+	};
 
-  oneLiner?: string
-  status: 'failed' | 'quit' | 'active' | 'acquired'
-}
+	oneLiner?: string;
+	status: "closed" | "quit" | "active" | "acquired";
+};
 
 export const exp: expProps[] = [
-  {
-    id: uuidv4(),
-    dates: '03.2024',
-    company: {
-      name: 'Stravisuals',
-      href: 'https://stravisuals.com',
-    },
-    oneLiner: 'Visuals for Strava Activities',
-    status: 'active',
-  },
-  {
-    id: uuidv4(),
-    dates: '03.2024',
-    company: {
-      name: 'Goose Game',
-      href: 'https://quack.kyzo.io',
-    },
-    oneLiner: '🪿 Game',
-    status: 'active',
-  },
-  {
-    id: uuidv4(),
-    dates: '02.2025',
-    company: {
-      name: 'fluar',
-      href: 'https://fluar.com',
-    },
-    oneLiner: 'AI-powered data enrichment',
-    status: 'active',
-  },
-  {
-    id: uuidv4(),
-    dates: '01.2024',
-    company: {
-      name: 'copycopter',
-      href: 'https://copycopter.ai',
-    },
-    oneLiner: 'generate AI videos from simple text prompts',
-    status: 'acquired',
-  },
-  {
-    id: uuidv4(),
-    dates: '08.2023',
-    company: {
-      name: 'Yove',
-      href: 'https://www.hiyove.com',
-    },
-    oneLiner: 'reach your personal goals with a little help from AI',
-    status: 'failed',
-  },
+	{
+		id: uuidv4(),
+		dates: "03.2024",
+		company: {
+			name: "Stravisuals",
+			href: "https://stravisuals.com",
+		},
+		oneLiner: "Visuals for Strava Activities",
+		status: "active",
+	},
+	{
+		id: uuidv4(),
+		dates: "03.2024",
+		company: {
+			name: "Goose Game",
+			href: "https://quack.kyzo.io",
+		},
+		oneLiner: "🪿 Game",
+		status: "active",
+	},
+	{
+		id: uuidv4(),
+		dates: "02.2025",
+		company: {
+			name: "fluar",
+			href: "https://fluar.com",
+		},
+		oneLiner: "AI-powered data enrichment",
+		status: "active",
+	},
+	{
+		id: uuidv4(),
+		dates: "01.2024",
+		company: {
+			name: "copycopter",
+			href: "https://copycopter.ai",
+		},
+		oneLiner: "generate AI videos from simple text prompts",
+		status: "acquired",
+	},
+	{
+		id: uuidv4(),
+		dates: "08.2023",
+		company: {
+			name: "Yove",
+			href: "https://www.hiyove.com",
+		},
+		oneLiner: "reach your personal goals with a little help from AI",
+		status: "closed",
+	},
 
-  {
-    id: uuidv4(),
-    dates: '06.2023',
-    company: {
-      name: 'finey',
-      href: 'https://finey.io',
-    },
-    oneLiner: 'classify, sort and categorize your invoices automatically',
+	{
+		id: uuidv4(),
+		dates: "06.2023",
+		company: {
+			name: "finey",
+			href: "https://finey.io",
+		},
+		oneLiner: "classify, sort and categorize your invoices automatically",
 
-    status: 'failed',
-  },
-  {
-    id: uuidv4(),
-    dates: '06.2023',
-    company: {
-      name: 'flyfile',
-      href: 'https://flyfile.io',
-    },
+		status: "closed",
+	},
+	{
+		id: uuidv4(),
+		dates: "06.2023",
+		company: {
+			name: "flyfile",
+			href: "https://flyfile.io",
+		},
 
-    oneLiner: 'sort out your email attachments automatically',
-    status: 'failed',
-  },
-  {
-    id: uuidv4(),
-    dates: '07.2020-02.2023',
-    title: 'COO & co-founder',
-    company: {
-      name: 'epinote',
-      href: 'https://epinote.io',
-    },
-    oneLiner: 'automated remote workforce management platform. raised $1.6m',
-    status: 'quit',
-  },
-]
+		oneLiner: "sort out your email attachments automatically",
+		status: "closed",
+	},
+	{
+		id: uuidv4(),
+		dates: "07.2020-02.2023",
+		title: "COO & co-founder",
+		company: {
+			name: "epinote",
+			href: "https://epinote.io",
+		},
+		oneLiner: "automated remote workforce management platform. raised $1.6m",
+		status: "quit",
+	},
+];
