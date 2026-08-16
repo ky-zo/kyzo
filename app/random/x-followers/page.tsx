25fc1744d560cf2f1eaf9ca135fe138db9c4ceb7
Kamil Zowczak <zowczak.kamil@gmail.com>
2026-08-15 19:04:10 -0700

    Restore responsive follower slider motion

--- app/random/x-followers/page.tsx
+++ app/random/x-followers/page.tsx
@@ -13,7 +13,7 @@ export default function XFollowersPage() {
     <main className="w-full max-w-md">
       <iframe
         className="block h-[590px] w-full border-0"
-        src="/follower-counter/index.html?v=drag-synced-slider-2026-08-15"
+        src="/follower-counter/index.html?v=balanced-slider-motion-2026-08-15"
         title="kyzo follower growth"
       />
     </main>
